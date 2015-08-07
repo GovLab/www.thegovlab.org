991becefa9b83b92f35fb5251b6c031a5da5e941
Claudio Medonca <claudioccm@Claudios-MacBook-Pro-2.local>
2015-08-07 14:22:23 -0400

    First commit.
    First version of homepage. Responsive

---
+++ static/js/scripts.js
@@ -0,0 +1,50 @@
+$(document).ready(function($) {
+   
+$('.js-close').click(function() {
+    $('.b-sticky').fadeOut('fast');
+});
+
+// Main nav logic
+$('.js-nav-trigger').click(function() {
+    $('.b-main-menu').addClass('m-active');
+    $('#overlay').addClass('m-active');
+});
+
+$('#overlay').click(function() {
+    $('.b-main-menu').removeClass('m-active');
+    $(this).removeClass('m-active');
+});
+
+
+// Search Logic
+$('.js-search-trigger').click(function() {
+    $('.e-search').addClass('m-active');
+    $('#overlay').addClass('m-active');
+});
+
+$('#overlay').click(function() {
+    $('.e-search').removeClass('m-active');
+    $(this).removeClass('m-active');
+});
+
+
+
+
+
+
+
+
+
+
+// Sample Click Feature with overlay
+// $('.triggerClass').click(function() {
+//     $('.menuClass').toggleClass('m-active');
+//     $('#overlay').toggleClass('m-active');
+// });
+
+// $('#overlay').click(function() {
+//     $('.menuClass').removeClass('m-active');
+//     $(this).removeClass('m-active');
+// });
+
+});
