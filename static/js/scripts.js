7e8a39a15be04734c71840b174d584dc193ce987
Anirudh Dinesh <anirudhdinesh@Anirudhs-MBP.fios-router.home>
2019-12-09 13:43:19 -0500

    new-blog-features

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -297,8 +297,11 @@ var render = function(posts) {
     });
 };
 
+
 // Note that this assumes that thegovlab.org has CORS headers.
-$.get('http://thegovlab.org/featured-website/feed/', function(xml) {
+//This is only temporary to pull specific blog posts instead of the latest
+// $.get('http://thegovlab.org/featured-website/feed/', function(xml) {
+    $.get('http://thegovlab.org/website-feature-temp/feed/', function(xml) {
     var posts = [];
     $('item', xml).each(function() {
         posts.push({
